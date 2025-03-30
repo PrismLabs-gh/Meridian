2533cf4e4fc024641772f7193ba5b026ce7c44a1
spaced <73479844+spacedouut@users.noreply.github.com>
2025-03-30 15:01:38 -0400

    routing setup, remove unneeded assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { Greeting } from './components/Greeting/Greeting'
+import { Routes, Route } from 'react-router-dom'
+
+
+function HomepageWrapper() {
+  return (
+    <div className="homepage">
+      <div className="page-content">
+        <Greeting username='user'/>
+      </div>
+    </div>
+  )
+}
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <Greeting username="user"/>
-    </>
+    <Routes>
+      <Route path="/" element={<HomepageWrapper />} />
+    </Routes>
   )
 }
 
