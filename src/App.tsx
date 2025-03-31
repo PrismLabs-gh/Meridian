a73f0c367000726c41aa84a85041f38cc171e47c
spaced <73479844+spacedouut@users.noreply.github.com>
2025-03-30 20:34:22 -0400

    Various changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,30 @@
-import { useState } from 'react'
-import './App.css'
-import { Greeting } from './components/Greeting/Greeting'
-import { Routes, Route } from 'react-router-dom'
-
+import { useState } from "react";
+import "./App.css";
+import { Greeting } from "./components/Greeting/Greeting";
+import { Routes, Route } from "react-router-dom";
+import { Composer } from "./components/Composer/Composer";
 
 function HomepageWrapper() {
   return (
     <div className="homepage">
-      <div className="page-content">
-        <Greeting username='user'/>
+      <div className="homepage-container">
+        <div className="homepage-content">
+          <Greeting username="user" />
+          <Composer />
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <Routes>
       <Route path="/" element={<HomepageWrapper />} />
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
